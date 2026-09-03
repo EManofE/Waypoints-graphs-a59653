a413c23161065f01b31505f40a5c5d422c9d31f6
EManofE <thepreacherofe@gmail.com>
2026-09-03 22:06:42 +0100

    Commanding a path Step 1

--- Assets/scripts/follow.cs
+++ Assets/scripts/follow.cs
@@ -7,7 +7,7 @@ public class follow : MonoBehaviour
 
     Transform goal;
     float speed = 5f;
-    float accuracy = 1f;
+    float accuracy = 10f;
     float rotspeed = 1f;
 
     public GameObject wpmanager;
@@ -15,12 +15,15 @@ public class follow : MonoBehaviour
     GameObject currentnode;
     int currentwp = 0;
     Graph g;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         wps = wpmanager.GetComponent<wpmanager>().waypoints;
         g = wpmanager.GetComponent<wpmanager>().graph;
         currentnode = wps[0];
+
+        Invoke("GotoHeli", 3);
+
     }
 
 
@@ -45,8 +48,26 @@ public class follow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(g.pathlist.Count == 0 || currentwp == g.pathlist.Count)
+        return;
+
+        if(Vector3.Distance(g.pathlist[currentwp].getId().transform.position, this.transform.position)< accuracy)
+        {
+            currentnode = g.pathlist[currentwp].getId();
+            currentwp++;
+
+        }
+
+        if(currentwp < g.pathlist.Count)
+        {
+            goal = g.pathlist[currentwp].getId().transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x,this.transform.position.y,this.transform.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,Quaternion.LookRotation(direction),Time.deltaTime * rotspeed);
+            this.transform.Translate(0,0,speed * Time.deltaTime);
+        }
     }
+
 }
