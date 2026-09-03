610d99648ed82aed30e456c09e8aaf65b9b28041
EManofE <thepreacherofe@gmail.com>
2026-09-03 19:20:35 +0100

    Circuit e Tracker

--- Assets/scripts/followwp.cs
+++ Assets/scripts/followwp.cs
@@ -7,23 +7,41 @@ public class followwp : MonoBehaviour
     int currentwp = 0;
 
     public float speed = 10.0f;
-    public float rotspeed = 1f;
+    public float rotspeed;
+    public float lookahead = 10f;
+
+    GameObject tracker;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+    }
+
+
+    void ProgressTracker()
+    {
+
+        if(Vector3.Distance(tracker.transform.position, this.transform.position)>lookahead) return;
         
+        if(Vector3.Distance(this.transform.position, waypoints[currentwp].transform.position)<10)
+        currentwp++;
+
+        if(currentwp>= waypoints.Length)
+        currentwp = 0;
+
+        tracker.transform.LookAt(waypoints[currentwp].transform);
+        tracker.transform.Translate(0,0,(speed +20)*Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(this.transform.position, waypoints[currentwp].transform.position)<10)
-        currentwp ++;
+        ProgressTracker();
 
-        if(currentwp>=waypoints.Length)
-        currentwp =0;
-
-        //this.transform.LookAt(waypoints[currentwp].transform);
+       // this.transform.LookAt(waypoints[currentwp].transform);
 
 
         Quaternion lookatwp= Quaternion.LookRotation(waypoints[currentwp].transform.position - this.transform.position);
